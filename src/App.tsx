46011e36866f4c3d5d1a08e6ef055dfb608c1193
Eugenia Kim <ek2213@ic.ac.uk>
2019-10-22 22:09:52 +0100

    add auth using amplify

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,49 @@ import Submit from './submit/Submit';
 import { Route, BrowserRouter } from "react-router-dom";
 import Bet from './bet/Bet';
 //import SignUp from './login/Signup';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+
+const usernameAttributes = 'Username';
+
+const signUpConfig = {
+  header: 'Welcome to EugiCap!',
+  hideAllDefaults: true,
+  defaultCountryCode: '44',
+  signUpFields: [
+    {
+      label: usernameAttributes,
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+    {
+      label: 'PhoneNumber',
+      key: 'phone_number',
+      required: true,
+      displayOrder: 3,
+      type: 'string'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 4,
+      type: 'string'
+    }
+  ]
+};
 
 class App extends React.Component {
   public render() {
@@ -26,4 +69,7 @@ class App extends React.Component {
   // private renderSignUp = (props: any) => (<Submit {...props} />)
 }
 
-export default App;
+export default withAuthenticator(App, true, undefined, undefined, {
+  signUpConfig,
+  usernameAttributes
+});
